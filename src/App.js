9a3afcdc53ad08b911e824800c377254efc539cf
Miao <D067954@global.corp.sap>
2017-01-06 17:07:23 +0100

    include next react-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import Router from 'react-router/BrowserRouter';
+import Match from 'react-router/Match';
+import Miss from 'react-router/Miss';
+
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/layout/HomePage';
+import SupportPage from './components/layout/SupportPage';
+import NotFoundPage from './components/layout/NotFoundPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <div>
+            <Match exactly pattern="/" component={HomePage} />
+            <Match pattern="/support" component={SupportPage} />
+            <Miss component={NotFoundPage} />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/layout/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <h3>
+        Home
+      </h3>
+    );
+  }
+}
+
+export default HomePage;
